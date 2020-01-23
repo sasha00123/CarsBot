14b2dbdc1f3acca756eea95d2f37179c2c6636bf
Alexander Batyrgariev <sasha@bashkort.org>
2020-01-23 18:48:29 +0300

    Heroku static

--- cars/settings/heroku.py
+++ cars/settings/heroku.py
@@ -5,3 +5,10 @@ DEBUG = False
 DATABASES = {
     'default': dj_database_url.config()
 }
+
+MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware',]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
